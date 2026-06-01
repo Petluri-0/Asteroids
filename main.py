118f84f2f2ebaa45a0a5ff7ec66fc77665b861b8
Petluri-0 <petluriprasanth8@gmail.com>
2026-06-01 19:29:30 +0530

    Add Player class to represent the player which is inherited from Circleshape

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import pygame
-from logger import log_state
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
+import pygame
+from player import Player
+from logger import log_state
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player = Player(x,y)
     while(True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         #print(dt)
