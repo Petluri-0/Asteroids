56823931e730da5ecd4657e6b5a388c83489345f
Petluri-0 <petluriprasanth8@gmail.com>
2026-06-07 11:05:42 +0530

    split mechanism implemented for Big and medium Asteroids

--- asteroids.py
+++ asteroids.py
@@ -1,5 +1,8 @@
 import pygame
+import random
+from logger import log_event
 from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS
 from circleshape import CircleShape
 class Asteroids(CircleShape):
     def __init__(self, x:float,y:float,radius:float) ->None:
@@ -8,3 +11,19 @@ class Asteroids(CircleShape):
         pygame.draw.circle(screen,"white",self.position,self.radius,LINE_WIDTH)
     def update(self, dt:float):
         self.position+=self.velocity*dt
+    
+    def split(self):
+        self.kill()
+        if(self.radius <= ASTEROID_MIN_RADIUS):
+            return
+        else:
+            log_event("asteroid_split")
+            angle = random.uniform(20,50)
+            first_asteroid_movement = self.velocity.rotate(angle)
+            second_asteroid_movement = self.velocity.rotate(-angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroids(self.position.x,self.position.y,new_radius)
+            asteroid2 = Asteroids(self.position.x,self.position.y,new_radius)
+            asteroid1.velocity = first_asteroid_movement * 1.2
+            asteroid2.velocity = second_asteroid_movement * 1.2
+
--- main.py
+++ main.py
@@ -35,6 +35,11 @@ def main():
 
         updatable.update(dt)
         for asteroid in asteroids:
+            for shot in shots:
+                if(shot.collides_with(asteroid)):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
             if(asteroid.collides_with(player)):
                 log_event("player_hit")
                 print("Game over!")
